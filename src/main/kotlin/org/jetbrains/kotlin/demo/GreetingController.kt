11adf4e6d0c55d2e974713a831830bc89f50d1fb
Takahiro Suzuki <t.suzuki@techd.jp>
2018-04-06 17:00:36 +0900

    add auth0

--- src/main/kotlin/org/jetbrains/kotlin/demo/GreetingController.kt
+++ src/main/kotlin/org/jetbrains/kotlin/demo/GreetingController.kt
@@ -5,12 +5,17 @@ import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import java.util.concurrent.atomic.AtomicLong
 
+
 @RestController
 class GreetingController {
 
     val counter = AtomicLong()
 
-    @GetMapping("/greeting")
-    fun greeting(@RequestParam(value = "name", defaultValue = "World") name: String) =
+    @GetMapping("/public/greeting")
+    fun publicGreeting(@RequestParam(value = "name", defaultValue = "World") name: String) =
+            Greeting(counter.incrementAndGet(), "Hello, $name")
+
+    @GetMapping("/private/greeting")
+    fun privateGreeting(@RequestParam(value = "name", defaultValue = "World") name: String) =
             Greeting(counter.incrementAndGet(), "Hello, $name")
 }
